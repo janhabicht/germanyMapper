8ed6630740dabc60e8800d29e8a3f2a5fd6d56bd
Jan <jan@cpmsys.de>
2019-08-18 13:53:41 +0200

    border hover

--- src/store.js
+++ src/store.js
@@ -5,7 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    bavaria:{},
+    bremen:{},
+    hamburg:{},
+    hesse:{},
+    saxony:{},
+    thuringia:{},
+    saxony_a:{},
+    baden_w:{},
+    berlin:{},
+    l_saxony:{},
+    mecklenburg:{},
+    nrw:{},
+    rhineland_p:{},
+    saarland:{},
+    schleswig_h:{},
+    brandenburg:{},
   },
   mutations: {
 
